ffc36e475c415241a607896f7d3c0d4994c651cc
Vicente Gutiérrez <gtz.rvera@gmail.com>
2022-06-21 20:38:11 -0500

    Update cognito configuration

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_AcKeIqk4w', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4rq3044t2lb9eprvv8usrrc297', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
